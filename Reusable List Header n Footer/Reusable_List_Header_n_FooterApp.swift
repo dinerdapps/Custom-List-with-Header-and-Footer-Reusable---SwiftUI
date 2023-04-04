60f2ba62edbc4d331e8f5de9de7654c90315666c
DiNerdyYawdie <62502925+DiNerdyYawdie@users.noreply.github.com>
2023-04-04 16:44:30 -0400

    Initial Commit

---
+++ Reusable List Header n Footer/Reusable_List_Header_n_FooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reusable_List_Header_n_FooterApp.swift
+//  Reusable List Header n Footer
+//
+//  Created by Chad on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Reusable_List_Header_n_FooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
